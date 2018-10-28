42413c4c19f0c479695e50c8c50c244913cd92eb
030 <chocolatey030@gmail.com>
2018-10-28 22:17:47 +0100

    [GH-41] ensure that ci fails if newer version of firefox is available

---
+++ latest_version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+goyq() {
+	wget https://github.com/030/go-yq/releases/download/1.0.0/go-yg-1.0.0-linux
+	chmod +x go-yg-1.0.0-linux
+}
+
+compare() {
+	readonly LATEST_VERSION=$(curl -s https://product-details.mozilla.org/1.0/firefox_versions.json | jq -r .LATEST_FIREFOX_VERSION)
+	readonly VERSION=$(./go-yg-1.0.0-linux -yamlFile /home/ben/dev/ansible-firefox/defaults/main.yml -key firefox_version)
+	
+	if [[ "$LATEST_VERSION" != "$VERSION" ]]; then
+		echo "A newer version of Firefox is available ($LATEST_VERSION vs. $VERSION)!"
+		exit 1
+	fi
+}
+
+main() {
+	goyq
+        compare
+}
+
+main
