0e7f592f50a6c5298721fd80c80ced2005e8d5ef
030 <chocolatey030@gmail.com>
2018-10-28 22:28:36 +0100

    [GH-41] ensure that defaults/main.yml file is checked

--- latest_version.sh
+++ latest_version.sh
@@ -9,7 +9,7 @@ goyq() {
 
 compare() {
 	readonly LATEST_VERSION=$(curl -s https://product-details.mozilla.org/1.0/firefox_versions.json | jq -r .LATEST_FIREFOX_VERSION)
-	readonly VERSION=$(./go-yg-1.0.0-linux -yamlFile /home/ben/dev/ansible-firefox/defaults/main.yml -key firefox_version)
+	readonly VERSION=$(./go-yg-1.0.0-linux -yamlFile defaults/main.yml -key firefox_version)
 	
 	if [[ "$LATEST_VERSION" != "$VERSION" ]]; then
 		echo "A newer version of Firefox is available ($LATEST_VERSION vs. $VERSION)!"
