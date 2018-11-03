5a4170b013e4f6d9787bb1698a839e2dba0f9faf
030 <chocolatey030@gmail.com>
2018-11-03 22:15:08 +0100

    [GH-43] go-yg should be go-yq

--- latest_version.sh
+++ latest_version.sh
@@ -4,14 +4,14 @@ set -e
 
 goyq() {
 	readonly GOYQ_VERSION=1.1.0
-	wget https://github.com/030/go-yq/releases/download/${GOYQ_VERSION}/go-yg-${GOYQ_VERSION}-linux
-	chmod +x go-yg-${GOYQ_VERSION}-linux
-	mv go-yg-1.0.0-linux go-yq
+	wget https://github.com/030/go-yq/releases/download/${GOYQ_VERSION}/go-yq-${GOYQ_VERSION}-linux
+	chmod +x go-yq-${GOYQ_VERSION}-linux
+	mv go-yq-1.0.0-linux go-yq
 }
 
 compare() {
 	readonly LATEST_VERSION=$(curl -s https://product-details.mozilla.org/1.0/firefox_versions.json | jq -r .LATEST_FIREFOX_VERSION)
-	readonly VERSION=$(./go-yg -yamlFile defaults/main.yml -key firefox_version)
+	readonly VERSION=$(./go-yq -yamlFile defaults/main.yml -key firefox_version)
 	readonly LATEST_CHECKSUM="sha512:"$(curl https://ftp.mozilla.org/pub/firefox/releases/${LATEST_VERSION}/SHA512SUMS | grep linux-x86_64/en-US/firefox-${VERSION}.tar.bz2 | sed -e "s|  linux-x86_64/en-US/firefox-${LATEST_VERSION}.tar.bz2$||g")
 	readonly CHECKSUM=$(./go-yq -yamlFile defaults/main.yml -key firefox_checksum)
 
