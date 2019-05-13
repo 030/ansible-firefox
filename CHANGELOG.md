6814c2985fdb4c86f90eb752b98709b29b3a3c10
030 <chocolatey030@gmail.com>
2019-05-13 16:20:55 +0200

    [GH-65] changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [3.0.1] - 2019-05-13
+### Changed
+- Firefox version 66.0.5.
+- Use docker go-yq to check latest version of firefox.
+
+## [3.0.0] - 2019-05-06
+### Changed
+- Default installation directory changed to /opt.
+- Firefox version 66.0.4.
+
+[Unreleased]: https://github.com/030/n3dr/compare/3.0.1...HEAD
+[3.0.1]: https://github.com/030/n3dr/releases/tag/3.0.1
+[3.0.0]: https://github.com/030/n3dr/releases/tag/3.0.0
--- latest_version.sh
+++ latest_version.sh
@@ -2,25 +2,6 @@
 
 set -e
 
-variables() {
-	readonly GOYQ_VERSION=2.0.1
-	readonly GOYQ_VERSION_BINARY=go-yq
-
-	if [ "$(uname)" == "Darwin" ]; then
-		echo "OS not supported"
-		exit 1
-	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-		GOYQ_VERSION_BINARY_OS="${GOYQ_VERSION_BINARY}-linux"
-	elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-		echo "OS not supported"
-		exit 1
-	elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-		GOYQ_VERSION_BINARY_OS="${GOYQ_VERSION_BINARY}-windows.exe"
-	fi
-
-	readonly GOYQ_CHECKSUM_TXT=${GOYQ_VERSION_BINARY_OS}.sha512.txt
-}
-
 compare_and_exit_if_required() {
 	if [[ "$1" != "$2" ]]; then
 		echo "Mismatch: $1 vs. $2"
@@ -28,29 +9,17 @@ compare_and_exit_if_required() {
 	fi
 }
 
-goyq() {
-	curl --location https://github.com/030/go-yq/releases/download/${GOYQ_VERSION}/${GOYQ_CHECKSUM_TXT} -o ${GOYQ_CHECKSUM_TXT}
-	curl --location https://github.com/030/go-yq/releases/download/${GOYQ_VERSION}/${GOYQ_VERSION_BINARY_OS} -o $GOYQ_VERSION_BINARY_OS
-
-	sha512sum --check $GOYQ_CHECKSUM_TXT
-
-	chmod +x $GOYQ_VERSION_BINARY_OS
-	mv $GOYQ_VERSION_BINARY_OS go-yq
-}
-
 compare() {
 	readonly LATEST_VERSION=$(curl -s https://product-details.mozilla.org/1.0/firefox_versions.json | jq -r .LATEST_FIREFOX_VERSION)
-	readonly VERSION=$(./go-yq .firefox_version defaults/main.yml)
+	readonly VERSION=$(docker run -v ${PWD}:/ansible-firefox utrecht/go-yq:2.1.0 .firefox_version /ansible-firefox/defaults/main.yml)
 	readonly LATEST_CHECKSUM=sha512:$(curl https://ftp.mozilla.org/pub/firefox/releases/${LATEST_VERSION}/SHA512SUMS | grep linux-x86_64/en-US/firefox-${VERSION}.tar.bz2 | sed -e "s|  linux-x86_64/en-US/firefox-${LATEST_VERSION}.tar.bz2$||g")
-	readonly CHECKSUM=$(./go-yq .firefox_checksum defaults/main.yml )
+	readonly CHECKSUM=$(docker run -v ${PWD}:/ansible-firefox utrecht/go-yq:2.1.0 .firefox_checksum /ansible-firefox/defaults/main.yml)
 
 	compare_and_exit_if_required $LATEST_VERSION $VERSION
 	compare_and_exit_if_required $LATEST_CHECKSUM $CHECKSUM
 }
 
 main() {
-	variables
-	goyq
 	compare
 }
 
